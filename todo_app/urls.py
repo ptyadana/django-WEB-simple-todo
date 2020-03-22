4bad34e14d114893d13fe22d7dae9af5892d835b
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-03-22 22:22:32 +0800

    add task html,views,urls add action

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from todo_app import views
 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('add',views.add_task,name='addtask'),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Task
 
 # Create your views here.
 def home(request):
     tasks = Task.objects.all()
     return render(request,'todo_app/home.html',{'tasks':tasks})
+
+
+def add_task(request):
+    if request.method == "POST":
+        name = request.POST.get('name','')
+        priority = request.POST.get('priority','')
+
+        task = Task(name=name,priority=priority)
+        task.save()
+
+        #return to home page
+        return redirect('/')
+
+    return render(request,'todo_app/add_task.html',{})
