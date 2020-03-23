e5b63c92f0053e7bd84aa2c03cd1f2c4fba024af
Phone Thiri Yadana <ptyadana@users.noreply.github.com>
2020-03-23 11:54:18 +0800

    change heroku app name

--- todo/settings.py
+++ todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE')=='True')
 
-ALLOWED_HOSTS = ['simply-todo-by-ptyadana.herokuapp.com']
+ALLOWED_HOSTS = ['simply-todo-app-by-ptyadana.herokuapp']
 
 
 # Application definition
